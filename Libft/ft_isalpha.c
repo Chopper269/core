398d2a006cda386371ac7a4728d7c56db9c7a6c6
Chopper269 <151777540+Chopper269@users.noreply.github.com>
2024-02-21 19:33:22 +0100

    Update ft_isalpha.c

--- Libft/ft_isalpha.c
+++ Libft/ft_isalpha.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 //Desarrollo de la funcion
-int ft_isalpha(int c) {
+int ft_isalpha(int c) { //la funcion va a operar con enteros, y se define que la variable c es entero
     if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) { //Si el caracter es menor o igual a 64 y mayor o igual a 91, o es menor o igual a 97 y mayor o igual a 122∫∫∫∫∫∫)
         return 1; // El caracter es una letra mayuscula o minuscula
     } else {
@@ -12,21 +12,21 @@ int ft_isalpha(int c) {
 }
 
 //Desarrollo del main
-int main()
+int main() //declaracion de main. Es un entero porque retorna un 0 al final, y es void porque no tiene argumentos de entrada
 {
 
     //Declaracion de variables
-    char entrada;
-    int resultado;
-    int resultado2;
+    char entrada; //define que la variable entrada es un char
+    int resultado; //define que la variable resultado es un int
+    int resultado2; //define que la variable resultado es un int
 
     //Asignacion de valores a variables
-    entrada = 'j';
-    resultado = ft_isalpha (entrada);
-    resultado2 = isalpha (entrada);
+    entrada = 'j'; //asigna un valor de entrada
+    resultado = ft_isalpha (entrada); //asigna un valor a resultado ejecutando ft_isalpha con la variable de entrada
+    resultado2 = isalpha (entrada); //asigna un valor a resultado2 ejecutando isalpha con la variable de entrada
 
     //Ejecucion del programa
-    printf("resultado mio %d\n", resultado);
-    printf("resultado original %d\n", resultado2);
-    return 0;
+    printf("resultado mio %d\n", resultado); //muestra por pantalla el resultado con ft_isalpha
+    printf("resultado original %d\n", resultado2); //muestra por pantalla el resultado con isalpha
+    return 0; //fin del programa
 }
