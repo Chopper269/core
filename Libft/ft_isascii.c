2a9f2b8465ff8a537d6b0d87009a44d7e525bf5e
chopper269 <rubenmartinroyo@outlook.com>
2024-02-24 20:13:31 +0100

    COPYPASTEO

--- Libft/ft_isascii.c
+++ Libft/ft_isascii.c
@@ -6,7 +6,7 @@
 /*   By: ruben <ruben@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 19:55:40 by ruben             #+#    #+#             */
-/*   Updated: 2024/02/24 20:10:46 by ruben            ###   ########.fr       */
+/*   Updated: 2024/02/24 20:13:04 by ruben            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,12 @@
 //Inclusion de headers
 #include <ctype.h>
 #include <stdio.h>
+
+//Desarrollo de la funcion
+int ft_isdigit(int c) {//la funcion va a operar con enteros, y se define que la variable c es entero
+    if (c >= 48 && c <= 59) { //Si el caracter es mayor o igual a 0 y menor o igual a 9
+        return 1; // El caracter es un numero
+    } else { // Y si no
+        return 0; // El caracter no es un numero
+    }
+}
--- Libft/ft_isdigit.c
+++ Libft/ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: ruben <ruben@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 19:58:32 by ruben             #+#    #+#             */
-/*   Updated: 2024/02/24 20:10:55 by ruben            ###   ########.fr       */
+/*   Updated: 2024/02/24 20:12:36 by ruben            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <ctype.h>
 #include <stdio.h>
 
+//Desarrollo de la funcion
 int ft_isdigit(int c) {//la funcion va a operar con enteros, y se define que la variable c es entero
     if (c >= 48 && c <= 59) { //Si el caracter es mayor o igual a 0 y menor o igual a 9
         return 1; // El caracter es un numero
