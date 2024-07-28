e2bb8d0cf7955467343b57f6107867a3347e3a7f
chopper269 <rubenmartinroyo@outlook.com>
2024-07-28 19:36:41 +0200

    repasando

--- Libft/ft_isalpha.c
+++ Libft/ft_isalpha.c
@@ -35,7 +35,7 @@ int main() //declaracion de main. Es un entero porque retorna un 0 al final, y e
     int resultado2; //define que la variable resultado es un int
 
     //Asignacion de valores a variables
-    entrada = 'j'; //asigna un valor de entrada
+    entrada = '8'; //asigna un valor de entrada
     resultado = ft_isalpha (entrada); //asigna un valor a resultado ejecutando ft_isalpha con la variable de entrada
     resultado2 = isalpha (entrada); //asigna un valor a resultado2 ejecutando isalpha con la variable de entrada
 
