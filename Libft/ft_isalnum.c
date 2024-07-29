3f6d07c2ff2189c526249e73ff8e1a264f81d31c
chopper269 <rubenmartinroyo@outlook.com>
2024-07-29 17:19:26 +0200

    repaso de las funciones hechas hace mil

--- Libft/ft_isalnum.c
+++ Libft/ft_isalnum.c
@@ -19,9 +19,9 @@
 //Desarrollo de la funcion
 int ft_isalnum(int c) {//la funcion va a operar con enteros, y se define que la variable c es entero
     if ((c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122)) { //Si el caracter es numerico o minusculas, o mayusculas, en la tabla ascii
-        return 1; // El caracter es un numero
-    } else { // Y si no
-        return 0; // El caracter no es un numero
+        return 1; // devuelve 1
+    } else { // Y si no es alfanumerico
+        return 0; // devuelve 0
     }
 }
 
@@ -36,12 +36,12 @@ int main() {//declaracion de main. Es un entero porque retorna un 0 al final, y 
     int resultado2; //define que la variable resultado es un int
 
     //Asignacion de valores a variables
-    entrada = '5'; //asigna un valor de entrada
-    resultado = ft_isalnum (entrada); //asigna un valor a resultado ejecutando ft_isalpha con la variable de entrada
-    resultado2 = isalnum (entrada); //asigna un valor a resultado2 ejecutando isalpha con la variable de entrada
+    entrada = '5'; //asigna un valor de entrada, aqui puedes poner lo que quieras
+    resultado = ft_isalnum (entrada); //asigna un valor a resultado ejecutando ft_isalnum con la variable de entrada
+    resultado2 = isalnum (entrada); //asigna un valor a resultado2 ejecutando isalnum con la variable de entrada
 
     //Ejecucion del programa
-    printf("resultado mio %d\n", resultado); //muestra por pantalla el resultado con ft_isalpha
-    printf("resultado original %d\n", resultado2); //muestra por pantalla el resultado con isalpha
+    printf("resultado mio %d\n", resultado); //muestra por pantalla el resultado con ft_isalnum
+    printf("resultado original %d\n", resultado2); //muestra por pantalla el resultado con isalnum
     return 0; //fin del programa
 }
--- Libft/ft_isascii.c
+++ Libft/ft_isascii.c
@@ -18,9 +18,30 @@
 
 //Desarrollo de la funcion
 int ft_isascii(int c) {//la funcion va a operar con enteros, y se define que la variable c es entero
-    if (c >= 32 && c <= 126) { //Si el caracter esta en el rango de los caracteres ascii imprimibles
-        return 1; // El caracter es ascii imprimible
+    if (c >= 00 && c <= 126) { //Si el caracter esta en el rango de los caracteres ascii
+        return 1; // El caracter es ascii
     } else { // Y si no
-        return 0; // El caracter no es ascii imprimible
+        return 0; // El caracter no es ascii
     }
+}
+
+
+//Desarrollo del main
+int main() //declaracion de main. Es un entero porque retorna un 0 al final, y es void porque no tiene argumentos de entrada
+{
+
+    //Declaracion de variables
+    char entrada; //define que la variable entrada es un char
+    int resultado; //define que la variable resultado es un int
+    int resultado2; //define que la variable resultado es un int
+
+    //Asignacion de valores a variables
+    entrada = 'h'; //asigna un valor de entrada
+    resultado = ft_ascii (entrada); //asigna un valor a resultado ejecutando ft_isascii con la variable de entrada
+    resultado2 = isascii (entrada); //asigna un valor a resultado2 ejecutando isascii con la variable de entrada
+
+    //Ejecucion del programa
+    printf("resultado mio %d\n", resultado); //muestra por pantalla el resultado con ft_isascii
+    printf("resultado original %d\n", resultado2); //muestra por pantalla el resultado con isascii
+    return 0; //fin del programa
 }
