96e5bd4729afa1051fabd5d6f3a178dd74c33226
chopper269 <rubenmartinroyo@outlook.com>
2024-07-30 17:15:07 +0200

    strlen hecha

--- Libft/ft_strlen.c
+++ Libft/ft_strlen.c
@@ -1,4 +1,5 @@
-
+#include <stdio.h>
+#include <ctype.h>
 
 int ft_strlen(const char *str)      //La funcion va a devolver un valor de tipo entero, y recibira un puntero de tipo const char (str)
 
@@ -7,7 +8,7 @@ int ft_strlen(const char *str)      //La funcion va a devolver un valor de tipo 
 
     longitud = 0;                   //inicializamos la variable con valor 0.
 
-    while (srt[longitud] != '\0')   //cuando (while) srt (el valor que recibe la funcion) es diferente a 0, entra en el bucle (o sigue en el)
+    while (str[longitud] != '\0')   //cuando (while) srt (el valor que recibe la funcion) es diferente a 0, entra en el bucle (o sigue en el)
     
         longitud++;                 //este bucle solo hara que el tamaño de la variable logitud aumente +1 su valor hasta encontrar un caracter nulo
   
@@ -19,9 +20,9 @@ int ft_strlen(const char *str)      //La funcion va a devolver un valor de tipo 
 int main()
 
 {
-    char ejemplo[] = "pruebita";    //main de prueba, declara una variable ejemplo con la palabra a contar
+    char ejemplo[] = "hijo de mil putas";    //main de prueba, declara una variable ejemplo con la palabra a contar
 
-    printf (ft_strlen(ejemplo));    //imprime por pantalla la funcion ftstrlen con el valor ejemplo
+    printf ("%d \n", ft_strlen(ejemplo));    //imprime por pantalla la funcion ftstrlen con el valor ejemplo
 
     return(0);                      //finaliza el programa
 }
