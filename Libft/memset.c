e7b7634b14f5206a6673e379fc2809294120992a
Chopper269 <151777540+Chopper269@users.noreply.github.com>
2024-08-08 20:29:29 +0200

    probando

--- Libft/memset.c
+++ Libft/memset.c
@@ -1,4 +1,4 @@
 #include <ctype.h>
 #include <stdio.h>
 
-void    *ft_memset(void *a, int b, size_t c)    // a es la variable puntero, b es el argumento de
+void    *ft_memset(void *a, int b, size_t c)    // a es la variable puntero, b es el argumento de algo que no recuerdo
