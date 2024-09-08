4566ce4b1d8a01493130a4b78af92c2142f3fa8d
Chopper269 <151777540+Chopper269@users.noreply.github.com>
2024-09-08 20:30:48 +0200

    ahora lo entiendo

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -1,4 +1,16 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <stddef.h>
 
-void    *ft_memset(void *a, int b, size_t c)    // a es la variable puntero, b es el argumento de algo que no recuerdo 
+void    *ft_memset(void *a, int b, size_t c){   // a es la variable puntero al bloque de memoria X, b es el dato que vamos a "grabar" en el puntero y c es su tamanio.
+    unsigned char *p; //declaramos un puntero p sin signo
+    unsigned char grabar; //declaramos la variable grabar sin signo
+
+    *p = a; //le damos el valor de a al puntero p.
+    grabar = (unsigned char)b; //convertimos en unsigned char el valor de b y le damos ese valor a la variable grabar
+
+    while (c--) //ejecutamos el bucle tantas veces como c, decrementando el valor hasta llegar a 0, no ejecutando el bucle
+    {
+        
+    }
+}
