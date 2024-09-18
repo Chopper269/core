eb543f4bf5b2e9034c944fcc2a32c79b505129b6
chopper269 <rubenmartinroyo@outlook.com>
2024-09-19 01:34:28 +0200

    terminando memcpy

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -2,27 +2,15 @@
 #include <stdio.h>
 #include <stddef.h>
 
-void    *ft_memcpy(void *des, void *ori, size_t tam){   // Definimos las varibles destino y origen, y su tamanio. Son void porque puede ser cualquier dato. 
-    unsigned char *d = des;
-    const unsigned char *o = ori;
+void    *ft_memcpy(void *dest, const void *ori, size_t tam){   // Definimos las varibles destino y origen, y su tamanio. Son void porque puede ser cualquier dato. const es que es un valor constante, no varia
+    unsigned char *d = (unsigned char *)dest; //convertimos la variable dest en unsigned char y lo asignamos a un puntero *d
+    const unsigned char *o = (const unsigned char *)ori; //convertimos la constante ori en unsigned y lo asignamos un puntero *o. Tambien sera de tipo const para evitar errores
 
 for (size_t i = 0; i < tam; i++) { // inicializa un bucle donde mientras i sea menor a tamanio, se sigue ejecutando, copiando el codigo de des a ori
-    d[i] = o[i]; // asigna al puntero d el valor del puntero o
+    d[i] = o[i]; // asigna al puntero d el valor del puntero o tantas veces como tam
 
-}
-
-}
-
-
-
-
-
-
-void *memcpy(void *dest, const void *src, size_t n) {
-    unsigned char *d = dest;
-    const unsigned char *s = src;
-    for (size_t i = 0; i < n; i++) {
-        d[i] = s[i];
     }
-    return dest;
+    
+    return dest;  // Devuelve el puntero al destino
 }
+
