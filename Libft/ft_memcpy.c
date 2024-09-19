594c7111187eeddae5644b72378bce279a1782f0
chopper269 <rubenmartinroyo@outlook.com>
2024-09-19 22:46:30 +0200

    perokdise

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -1,6 +1,7 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stddef.h>
+#include <string.h>
 
 void    *ft_memcpy(void *dest, const void *ori, size_t tam){   // Definimos las varibles destino y origen, y su tamanio. Son void porque puede ser cualquier dato. const es que es un valor constante, no varia
     unsigned char *d = (unsigned char *)dest; //convertimos la variable dest en unsigned char y lo asignamos a un puntero *d
@@ -14,4 +15,25 @@ for (size_t i = 0; i < tam; i++) { // inicializa un bucle donde mientras i sea m
     return dest;  // Devuelve el puntero al destino
 }
 
-//aqui abajo va el main que hare mañana.
+
+// Desarrollo del main
+
+
+void *ft_memcpy(void *dest, const void *ori, size_t tam);
+
+int main() {
+
+
+    char origen1[] = "pa ke kiere save eso jajasalu2xdxd";
+    char destino1[20];
+
+
+    printf("variable de origen antes de copia: %s\n", origen1); 
+    printf("variable de destino antes de copia: %s\n", destino1);
+    
+    ft_memcpy(destino1, origen1, strlen(origen1) + 1);
+
+    printf("variable de origen despues de copia: %s\n", origen1);
+    printf("variable de destino despues de copia: %s\n", destino1);
+
+
