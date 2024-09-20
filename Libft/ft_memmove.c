46305edf9b5b74c93c361a2cdb34f631f007c5b9
chopper269 <rubenmartinroyo@outlook.com>
2024-09-20 22:10:00 +0200

    fino

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -3,7 +3,7 @@
 #include <stddef.h>
 #include <string.h>
 
-void    *ft_memcpy(void *dest, void *ori, size_t tam){   // Definimos las varibles destino y origen, y su tamanio. Son void porque puede ser cualquier dato. const es que es un valor constante, no varia
+void *ft_memmove(void *dest, const void *ori, size_t tam){   // Definimos las varibles destino y origen, y su tamanio. Son void porque puede ser cualquier dato. const es que es un valor constante, no varia
     unsigned char *d = (unsigned char *)dest; //convertimos la variable dest en unsigned char y lo asignamos a un puntero *d
     const unsigned char *o = (const unsigned char *)ori; //convertimos la constante ori en unsigned y lo asignamos un puntero *o. Tambien sera de tipo const para evitar errores
 
@@ -27,7 +27,7 @@ void    *ft_memcpy(void *dest, void *ori, size_t tam){   // Definimos las varibl
         printf("variable de origen antes de copia: %s\n", origen1); 
         printf("variable de destino antes de copia: %s\n", destino1);
         
-        ft_memmove(destino1, origen1, strlen(origen1)); //(ojo, el +1 es para copiar el caracter nulo de fin de la cadena)
+        ft_memmove(destino1, origen1, strlen(origen1) + 1); //(ojo, el +1 es para copiar el caracter nulo de fin de la cadena)
 
         printf("variable de origen despues de copia: %s\n", origen1);
         printf("variable de destino despues de copia: %s\n", destino1);
